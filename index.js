fae0d9eaa644fc383a8cf5a9a4ccc06ab9413357
Jose-cmd98 <jose1234roberto@gmail.com>
2021-09-02 22:19:34 -0300

    reading file from url

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const http = require('http'); //import from node to use this modules
+const fs = require('fs') //use to read an html file
+
+
+
+
 
 const hostname = "127.0.0.1"; //create an const to use localhost as a test
 const port = 3000; //choosing port 3000 to use from that
@@ -6,11 +11,18 @@ const port = 3000; //choosing port 3000 to use from that
 
 const server = http.createServer((req,res)=>{  //callback
 
+    console.log(req.headers);// 
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    fs.readFile('index.html', (err, data)=>{
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.write(data); //data has the content from html
+        return res.end(); //finish and serve to the user the req
+    })    //IT WORKS!
 
-    res.end('Hey there!')
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+
+    // res.end('Hey there!')
 })
 
 
