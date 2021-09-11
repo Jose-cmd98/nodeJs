738cf2d0c6ec8205e2ec546b00ce36402e090325
Jose-cmd98 <jose1234roberto@gmail.com>
2021-09-10 23:06:32 -0300

    'dataManipulation'

--- readFile/index.js
+++ readFile/index.js
@@ -6,5 +6,16 @@ const fs = require('fs');
 // })
 
 fs.readFile('read.txt', (err,data)=>{
-    console.log(data.toString());
+    // console.log(data.toString());
+
+    //manipulating data
+
+    let str = data.toString();
+
+    // let newStr = str.substr(0, 7);
+
+     let newStr = str.split('/'); //show as array
+
+    console.log(newStr);
 })
+
