f61c012806ae5112167d491bd6a04ca1c0f80971
Jose-cmd98 <jose1234roberto@gmail.com>
2021-09-12 19:17:44 -0300

    input|output

--- readFile/index.js
+++ readFile/index.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-
+// const fs = require('fs');
+const readline = require('readline');
 
 // fs.appendFile('read.txt','Jose', (err) =>{
 //     if(err) throw err;
@@ -28,7 +28,34 @@ fs.unlink('read.txt', (err)=>{
     console.log('Arquivo deletado!');
 })*/
 
-
+/* RENAME FILE
 fs.rename('read.txt', 'newRead.txt', (err)=>{
     console.log('Arquivo renomeado!');
+}) */
+
+//create interface to use input|Output
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 })
+
+//
+rl.question('Qual seu nome?', (name)=>{
+    console.log('O nome do Usuario é: ' +name);
+    rl.question('Qual sua idade?',(idade)=>{
+        console.log('A idade do '+name+ ' é ' +idade );
+
+    rl.question('Qual seu foco de estudo?',(focus)=>{
+        console.log('O sr.'+name+' de '+idade+' tem como foco de estudos '+focus);
+    })
+    })
+
+})
+
+
+//exit
+rl.on('close',()=>{
+    console.log('Finalizando!');
+    process.exit(0);
+})
+
