162ce408ce7554910137dc7a08884637c0564959
Jose-cmd98 <jose1234roberto@gmail.com>
2021-09-12 18:30:51 -0300

    Unlink/Rename

--- readFile/index.js
+++ readFile/index.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 
+
 // fs.appendFile('read.txt','Jose', (err) =>{
 //     if(err) throw err;
 //     console.log('IT WORKS!');
 // })
 
+//reading file 
+/*
 fs.readFile('read.txt', (err,data)=>{
     // console.log(data.toString());
 
@@ -17,5 +20,15 @@ fs.readFile('read.txt', (err,data)=>{
      let newStr = str.split('/'); //show as array
 
     console.log(newStr);
-})
+})*/
 
+
+/* =>to del an file
+fs.unlink('read.txt', (err)=>{
+    console.log('Arquivo deletado!');
+})*/
+
+
+fs.rename('read.txt', 'newRead.txt', (err)=>{
+    console.log('Arquivo renomeado!');
+})
