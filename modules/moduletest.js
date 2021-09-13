e801e43b280e64e8dd5178484870d5bd7f6f53c2
Jose-cmd98 <jose1234roberto@gmail.com>
2021-09-13 18:40:41 -0300

    moduleTest

---
+++ modules/moduletest.js
@@ -0,0 +1,6 @@
+//Creating function to use as module
+function NewModule(){
+    console.log('Module Works');
+}
+//exporting this to became a useage module
+exports.NewModule = NewModule;
---
+++ modules/testeM.js
@@ -0,0 +1,6 @@
+//using module created before
+const moduletest = require('./moduletest');
+
+
+//test module function to see if it works
+moduletest.NewModule();
